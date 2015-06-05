e667484bb2b211f241a77e65f84be843476f5fdb
Alexander Weidinger <alexwegoo@gmail.com>
2015-06-05 17:22:00 +0200

    implemented high layer functions for i2c and changed srf10 function calls accordingly.

---
+++ src/i2c_srf10/i2c.c
@@ -0,0 +1,77 @@
+#include <libopencm3/stm32/i2c.h>
+#include "i2c.h"
+
+static void i2c_send_start_and_wait_for_rdy(uint32_t i2c) {
+	/* start transaction */
+	i2c_send_start(i2c);
+	/* wait for START to finish and switch to master mode. */
+	while (!((I2C_SR1(i2c) & I2C_SR1_SB)
+		& (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
+}
+
+static void i2c_send_address_rw(uint32_t i2c, uint8_t address, uint8_t rw) {
+	uint32_t reg32 __attribute__((unused));
+	/* say to what address we want to talk to. */
+	/* yes, WRITE is correct - for selecting register */
+	i2c_send_7bit_address(i2c, address, rw);
+	/* enable ACK. */
+	i2c_enable_ack(i2c);
+	/* waiting for address is transferred. */
+	while(!(I2C_SR1(i2c) & I2C_SR1_ADDR));
+	/* cleaning ADDR condition sequence. */
+	reg32 = I2C_SR2(i2c);
+}
+
+static void i2c_send_register(uint32_t i2c, uint8_t reg) {
+	/* send desired register. */
+	i2c_send_data(i2c, reg);
+	while(!(I2C_SR1(i2c) & (I2C_SR1_BTF | I2C_SR1_TxE)));
+}
+
+void i2c_read_buffer(uint32_t i2c, uint8_t address, uint8_t reg,
+				uint8_t len, uint8_t *buffer) {
+	i2c_send_start_and_wait_for_rdy(i2c);
+	i2c_send_address_rw(i2c, address, I2C_WRITE);
+	i2c_send_register(i2c, reg);
+
+	/* send repeated start. */
+	i2c_send_start_and_wait_for_rdy(i2c);
+	/* send address again, but this time READ! */
+	i2c_send_address_rw(i2c, address, I2C_READ);
+
+	for(uint8_t i = 0; i < len; i++) {
+		/* last byte transfer. */
+		if(i == len - 1) {
+			/* disbale ACK. */
+			i2c_disable_ack(i2c);
+			buffer[i] = i2c_get_data(i2c);
+			/* NACK last byte. */
+			i2c_nack_current(i2c);
+		/* normal byte transfer. */
+		} else {
+			/* wait for byte transfer finish. */
+			while (!(I2C_SR1(i2c) & I2C_SR1_BTF));
+			buffer[i] = i2c_get_data(i2c);
+		}
+	}
+
+	/* send stop. */
+	i2c_send_stop(i2c);
+}
+
+void i2c_write_buffer(uint32_t i2c, uint8_t address, uint8_t reg,
+				uint8_t len, uint8_t *buffer) {
+	i2c_send_start_and_wait_for_rdy(i2c);
+	i2c_send_address_rw(i2c, address, I2C_WRITE);
+	i2c_send_register(i2c, reg);
+
+	for(uint8_t i = 0; i < len; i++) {
+		i2c_send_data(i2c, buffer[i]);
+		if(i != len - 1)
+			while (!(I2C_SR1(i2c) & I2C_SR1_BTF));
+		else
+			while(!(I2C_SR1(i2c) & (I2C_SR1_BTF | I2C_SR1_TxE)));
+	}
+
+	i2c_send_stop(i2c);
+}
--- src/i2c_srf10/srf10.c
+++ src/i2c_srf10/srf10.c
@@ -1,77 +1,36 @@
 #include <libopencm3/stm32/i2c.h>
+#include "srf10.h"
+#include "i2c.h"
 
 void srf10_start_measurement(uint32_t i2c, uint8_t sensor, uint8_t mode) {
-	uint32_t reg32 __attribute__((unused));
-
-	i2c_send_start(i2c);
-	while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-		& (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
-	i2c_send_7bit_address(i2c, sensor, I2C_WRITE);
-	while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
-	reg32 = I2C_SR2(i2c);
-	i2c_send_data(i2c, 0x00);
-	while (!(I2C_SR1(i2c) & I2C_SR1_BTF));
-	i2c_send_data(i2c, mode);
-	while (!(I2C_SR1(i2c) & (I2C_SR1_BTF | I2C_SR1_TxE)));
-	i2c_send_stop(i2c);
+	i2c_write_buffer(i2c, sensor, 0x00, 1, &mode);
 }
 
 uint8_t srf10_measurement_successful(uint32_t i2c, uint8_t sensor) {
-	uint32_t reg32 __attribute__((unused));
 	uint8_t result;
-
-	i2c_send_start(i2c);
-	while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-		& (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
-	i2c_send_7bit_address(i2c, sensor, I2C_WRITE);
-	while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
-	reg32 = I2C_SR2(i2c);
-	i2c_send_data(i2c, 0x00);
-	while (!(I2C_SR1(I2C1) & (I2C_SR1_BTF | I2C_SR1_TxE)));
-
-	i2c_send_start(i2c);
-	while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-		& (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
-	i2c_send_7bit_address(i2c, sensor, I2C_READ);
-	while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
-	reg32 = I2C_SR2(i2c);
-	while (!(I2C_SR1(i2c) & I2C_SR1_BTF));
-	result = i2c_get_data(i2c);
-	while (!(I2C_SR1(I2C1) & (I2C_SR1_BTF | I2C_SR1_TxE)));
-	i2c_send_stop(i2c);
+	i2c_read_buffer(i2c, sensor, 0x00, 1, &result);
 
 	return result == 0xff;
 }
 
 uint16_t srf10_get_last_measurement(uint32_t i2c, uint8_t sensor) {
-	uint32_t reg32 __attribute__((unused));
-	uint16_t result;
+        uint8_t buffer[2];
+        i2c_read_buffer(i2c, sensor, 0x02, 2, buffer);
 
-	i2c_send_start(i2c);
-	while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-		& (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
-	i2c_send_7bit_address(i2c, sensor, I2C_WRITE);
-	while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
-	reg32 = I2C_SR2(i2c);
-	i2c_send_data(i2c, 0x02);
-	while (!(I2C_SR1(i2c) & (I2C_SR1_BTF | I2C_SR1_TxE)));
+	uint16_t result = buffer[0];
+	result <<= 8;
+	result |= buffer[1];
 
-	i2c_send_start(i2c);
-	while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-		& (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
-	i2c_send_7bit_address(i2c, sensor, I2C_READ);
-	i2c_enable_ack(i2c);
-	i2c_nack_next(i2c);
-	while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
-	reg32 = I2C_SR2(i2c);
-	while (!(I2C_SR1(i2c) & I2C_SR1_BTF));
-	result = (uint16_t)(i2c_get_data(i2c) << 8);
-	i2c_disable_ack(i2c);
-	result |= i2c_get_data(i2c);
-	i2c_nack_current(i2c);
-	i2c_send_stop(i2c);
+        return result;
+}
 
-	return result;
+/*
+ * factor = range(0, 16)
+ *
+ * (temporary value)
+ */
+void srf10_change_gain(uint32_t i2c, uint8_t sensor, uint8_t factor) {
+	i2c_write_buffer(i2c, sensor, 0x01, 1, &factor);
 }
 
 /*
@@ -81,38 +40,6 @@ uint16_t srf10_get_last_measurement(uint32_t i2c, uint8_t sensor) {
  * (temporary value)
  */
 void srf10_change_range(uint32_t i2c, uint8_t sensor, uint8_t factor) {
-	uint32_t reg32 __attribute__((unused));
-
-	i2c_send_start(i2c);
-	while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-		& (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
-	i2c_send_7bit_address(i2c, sensor, I2C_WRITE);
-	while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
-	reg32 = I2C_SR2(i2c);
-	i2c_send_data(i2c, 0x02);
-	while (!(I2C_SR1(i2c) & I2C_SR1_BTF));
-	i2c_send_data(i2c, factor);
-	while (!(I2C_SR1(i2c) & (I2C_SR1_BTF | I2C_SR1_TxE)));
-	i2c_send_stop(i2c);
+	i2c_write_buffer(i2c, sensor, 0x02, 1, &factor);
 }
 
-/*
- * factor = range(0, 16)
- *
- * (temporary value)
- */
-void srf10_change_gain(uint32_t i2c, uint8_t sensor, uint8_t factor) {
-	uint32_t reg32 __attribute__((unused));
-
-	i2c_send_start(i2c);
-	while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-		& (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
-	i2c_send_7bit_address(i2c, sensor, I2C_WRITE);
-	while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
-	reg32 = I2C_SR2(i2c);
-	i2c_send_data(i2c, 0x01);
-	while (!(I2C_SR1(i2c) & I2C_SR1_BTF));
-	i2c_send_data(i2c, factor);
-	while (!(I2C_SR1(i2c) & (I2C_SR1_BTF | I2C_SR1_TxE)));
-	i2c_send_stop(i2c);
-}
