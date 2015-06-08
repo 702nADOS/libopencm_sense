649aec3c72543501ccd3735e99b3c738aadc0411
Alexander Weidinger <alexwegoo@gmail.com>
2015-06-08 11:17:28 +0200

    proper header files.

--- src/i2c_srf10/i2c.h
+++ src/i2c_srf10/i2c.h
@@ -1,4 +1,9 @@
+#ifndef _I2C_H_
+#define _I2C_H_
+
 #include <libopencm3/stm32/i2c.h>
 
 void i2c_read_buffer(uint32_t i2c, uint8_t address, uint8_t reg, uint8_t len, uint8_t *buffer);
 void i2c_write_buffer(uint32_t i2c, uint8_t address, uint8_t reg, uint8_t len, uint8_t *buffer);
+
+#endif
