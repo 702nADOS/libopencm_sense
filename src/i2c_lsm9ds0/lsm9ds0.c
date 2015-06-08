65b309a6dba81f379eda30add0ee421a2712048e
dk87 <drake87.dk@gmail.com>
2015-06-08 17:30:49 +0200

    few sensor components added

--- src/i2c_lsm9ds0/lsm9ds0.c
+++ src/i2c_lsm9ds0/lsm9ds0.c
@@ -2,14 +2,176 @@
 #include "lsm9ds0.h"
 #include "../i2c_srf10/i2c.h"
 
-void lsm9ds0_setup_temp(uint32_t i2c, uint8_t sensor)
+/* global variables */
+lsm9ds0AccelRange_t _accel_mg_lsb;
+lsm9ds0MagGain_t _mag_mgauss_lsb;
+lsm9ds0GyroScale_t _gyro_dps_digit;
+
+void lsm9ds0_enable_accel(uint32_t i2c, uint8_t sensor)
 {
-	uint8_t mode;
-	i2c_read_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG5_XM, 1, &mode);
-	mode |= (1<<7);		/* enable temperature sensor */
-	i2c_write_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG5_XM, 1, &mode);
+	// Enable the accelerometer continous
+	uint8_t reg_value;
+	reg_value = 0x67;	/* 100 hz XYZ */
+	i2c_write_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG1_XM,
+			 1, &reg_value);
+
+	reg_value = 0b11110000;
+	i2c_write_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG5_XM,
+			 1, &reg_value);
 }
 
+void lsm9ds0_enable_mag(uint32_t i2c, uint8_t sensor)
+{
+	// enable mag continuous
+	uint8_t reg_value = 0b00000000;
+	i2c_write_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG7_XM,
+			 1, &reg_value);
+}
+
+void lsm9ds0_enable_temp(uint32_t i2c, uint8_t sensor)
+{
+	uint8_t mode;
+	i2c_read_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG5_XM,
+			1, &mode);
+	mode |= (1<<7);		/* enable temperature sensor */
+	i2c_write_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG5_XM,
+			 1, &mode);
+}
+
+void lsm9ds0_enable_gyro(uint32_t i2c, uint8_t sensor)
+{
+	/* TODO */
+}
+
+lsm9ds0_setup_accel(uint32_t i2c, uint8_t sensor, lsm9ds0AccelRange_t range)
+{
+	uint8_t reg;
+	i2c_read_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG2_XM,
+			1, &reg);
+
+	reg &= ~(0b00111000);
+	reg |= range;
+	i2c_write_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG2_XM,
+			 1, &reg );
+
+	switch (range)
+	{
+	case LSM9DS0_ACCELRANGE_2G:
+		_accel_mg_lsb = LSM9DS0_ACCEL_MG_LSB_2G;
+		break;
+	case LSM9DS0_ACCELRANGE_4G:
+		_accel_mg_lsb = LSM9DS0_ACCEL_MG_LSB_4G;
+		break;
+	case LSM9DS0_ACCELRANGE_6G:
+		_accel_mg_lsb = LSM9DS0_ACCEL_MG_LSB_6G;
+		break;
+	case LSM9DS0_ACCELRANGE_8G:
+		_accel_mg_lsb = LSM9DS0_ACCEL_MG_LSB_8G;
+		break;
+	case LSM9DS0_ACCELRANGE_16G:
+		_accel_mg_lsb =LSM9DS0_ACCEL_MG_LSB_16G;
+		break;
+	}
+}
+
+lsm9ds0_setup_mag(uint32_t i2c, uint8_t sensor, lsm9ds0MagGain_t gain)
+{
+	uint8_t reg;
+	i2c_read_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG6_XM,
+			1, &reg);
+
+	reg &= ~(0b01100000);
+	reg |= gain;
+	i2c_write_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG6_XM,
+			 1, &reg);
+
+	switch(gain)
+	{
+	case LSM9DS0_MAGGAIN_2GAUSS:
+		_mag_mgauss_lsb = LSM9DS0_MAG_MGAUSS_2GAUSS;
+		break;
+	case LSM9DS0_MAGGAIN_4GAUSS:
+		_mag_mgauss_lsb = LSM9DS0_MAG_MGAUSS_4GAUSS;
+		break;
+	case LSM9DS0_MAGGAIN_8GAUSS:
+		_mag_mgauss_lsb = LSM9DS0_MAG_MGAUSS_8GAUSS;
+		break;
+	case LSM9DS0_MAGGAIN_12GAUSS:
+		_mag_mgauss_lsb = LSM9DS0_MAG_MGAUSS_12GAUSS;
+		break;
+	}
+}
+
+lsm9ds0_setup_temp(uint32_t i2c, uint8_t sensor)
+{
+	/* only for convenience */
+}
+
+lsm9ds0_setup_gyro(uint32_t i2c, uint8_t sensor, lsm9ds0GyroScale_t scale)
+{
+	uint8_t reg;
+	i2c_read_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG4_G,
+			1, &reg);
+
+	reg &= ~(0b00110000);
+	reg |= scale;
+	i2c_write_buffer(i2c, sensor, LSM9DS0_REGISTER_CTRL_REG4_G,
+			 1, &reg);
+
+	switch(scale)
+	{
+	case LSM9DS0_GYROSCALE_245DPS:
+		_gyro_dps_digit = LSM9DS0_GYRO_DPS_DIGIT_245DPS;
+		break;
+	case LSM9DS0_GYROSCALE_500DPS:
+		_gyro_dps_digit = LSM9DS0_GYRO_DPS_DIGIT_500DPS;
+		break;
+	case LSM9DS0_GYROSCALE_2000DPS:
+		_gyro_dps_digit = LSM9DS0_GYRO_DPS_DIGIT_2000DPS;
+		break;
+	}
+}
+
+void lsm9ds0_init_sensor(uint32_t i2c, uint8_t sensor)
+{
+	lsm9ds0_enable_accel(i2c,  sensor);
+	lsm9ds0_enable_mag(i2c, sensor);
+	lsm9ds0_enable_temp(i2c, sensor);
+	lsm9ds0_enable_gyro(i2c, sensor);
+
+	/* setup sensors */
+	lsm9ds0_setup_accel(i2c, sensor, LSM9DS0_ACCELRANGE_2G);
+	lsm9ds0_setup_mag(i2c, sensor, LSM9DS0_MAGGAIN_2GAUSS);
+	lsm9ds0_setup_temp(i2c, sensor);
+	lsm9ds0_setup_gyro(i2c, sensor, LSM9DS0_GYROSCALE_245DPS);
+}
+
+lsm9ds0Vector_t lsm9ds0_read_accel(uint32_t i2c, uint8_t sensor)
+{
+  // Read the accelerometer
+	lsm9ds0Vector_t accelData;
+	uint8_t buffer[6];
+	i2c_read_buffer(i2c, sensor, 0x80 | LSM9DS0_REGISTER_OUT_X_L_A,
+			6, buffer);
+
+	uint8_t xlo = buffer[0];
+	int16_t xhi = buffer[1];
+	uint8_t ylo = buffer[2];
+	int16_t yhi = buffer[3];
+	uint8_t zlo = buffer[4];
+	int16_t zhi = buffer[5];
+
+	// Shift values to create properly formed integer (low byte first)
+	xhi <<= 8; xhi |= xlo;
+	yhi <<= 8; yhi |= ylo;
+	zhi <<= 8; zhi |= zlo;
+
+	accelData.x = xhi;
+	accelData.y = yhi;
+	accelData.z = zhi;
+
+	return accelData;
+}
 
 uint16_t lsm9ds0_read_temp(uint32_t i2c, uint8_t sensor)
 {
--- src/i2c_lsm9ds0/lsm9ds0.h
+++ src/i2c_lsm9ds0/lsm9ds0.h
@@ -2,9 +2,42 @@
 #define __LSM9DS0_H__
 
 #define LSM9DS0_ADDRESS_ACCELMAG (0x1D) // 3B >> 1 = 7bit default
+#define LSM9DS0_ADDRESS_GYRO (0x6B) // D6 >> 1 = 7bit default
+
+// Linear Acceleration: mg per LSB
+#define LSM9DS0_ACCEL_MG_LSB_2G (0.061F)
+#define LSM9DS0_ACCEL_MG_LSB_4G (0.122F)
+#define LSM9DS0_ACCEL_MG_LSB_6G (0.183F)
+#define LSM9DS0_ACCEL_MG_LSB_8G (0.244F)
+#define LSM9DS0_ACCEL_MG_LSB_16G (0.732F) // Is this right? Was expecting 0.488F
+
+// Magnetic Field Strength: gauss range
+#define LSM9DS0_MAG_MGAUSS_2GAUSS      (0.08F)
+#define LSM9DS0_MAG_MGAUSS_4GAUSS      (0.16F)
+#define LSM9DS0_MAG_MGAUSS_8GAUSS      (0.32F)
+#define LSM9DS0_MAG_MGAUSS_12GAUSS     (0.48F)
+
+// Angular Rate: dps per LSB
+#define LSM9DS0_GYRO_DPS_DIGIT_245DPS      (0.00875F)
+#define LSM9DS0_GYRO_DPS_DIGIT_500DPS      (0.01750F)
+#define LSM9DS0_GYRO_DPS_DIGIT_2000DPS     (0.07000F)
 
 // Temperature: LSB per degree celsius
 #define LSM9DS0_TEMP_LSB_DEGREE_CELSIUS    (8)  // 1°C = 8, 25° = 200, etc.
+
+typedef enum
+{
+	LSM9DS0_REGISTER_WHO_AM_I_G          = 0x0F,
+	LSM9DS0_REGISTER_CTRL_REG1_G         = 0x20,
+	LSM9DS0_REGISTER_CTRL_REG3_G         = 0x22,
+	LSM9DS0_REGISTER_CTRL_REG4_G         = 0x23,
+	LSM9DS0_REGISTER_OUT_X_L_G           = 0x28,
+	LSM9DS0_REGISTER_OUT_X_H_G           = 0x29,
+	LSM9DS0_REGISTER_OUT_Y_L_G           = 0x2A,
+	LSM9DS0_REGISTER_OUT_Y_H_G           = 0x2B,
+	LSM9DS0_REGISTER_OUT_Z_L_G           = 0x2C,
+	LSM9DS0_REGISTER_OUT_Z_H_G           = 0x2D,
+} lsm9ds0GyroRegisters_t;
 
 typedef enum
 {
@@ -33,7 +66,57 @@ typedef enum
 	LSM9DS0_REGISTER_OUT_Z_H_A			  = 0x2D,
 } lsm9ds0MagAccelRegisters_t;
 
+typedef enum
+{
+	LSM9DS0_ACCELRANGE_2G                = (0b000 << 3),
+	LSM9DS0_ACCELRANGE_4G                = (0b001 << 3),
+	LSM9DS0_ACCELRANGE_6G                = (0b010 << 3),
+	LSM9DS0_ACCELRANGE_8G                = (0b011 << 3),
+	LSM9DS0_ACCELRANGE_16G               = (0b100 << 3)
+} lsm9ds0AccelRange_t;
+
+typedef enum
+{
+	LSM9DS0_ACCELDATARATE_POWERDOWN      = (0b0000 << 4),
+	LSM9DS0_ACCELDATARATE_3_125HZ        = (0b0001 << 4),
+	LSM9DS0_ACCELDATARATE_6_25HZ         = (0b0010 << 4),
+	LSM9DS0_ACCELDATARATE_12_5HZ         = (0b0011 << 4),
+	LSM9DS0_ACCELDATARATE_25HZ           = (0b0100 << 4),
+	LSM9DS0_ACCELDATARATE_50HZ           = (0b0101 << 4),
+	LSM9DS0_ACCELDATARATE_100HZ          = (0b0110 << 4),
+	LSM9DS0_ACCELDATARATE_200HZ          = (0b0111 << 4),
+	LSM9DS0_ACCELDATARATE_400HZ          = (0b1000 << 4),
+	LSM9DS0_ACCELDATARATE_800HZ          = (0b1001 << 4),
+	LSM9DS0_ACCELDATARATE_1600HZ         = (0b1010 << 4)
+} lm9ds0AccelDataRate_t;
+
+typedef enum
+{
+	LSM9DS0_MAGGAIN_2GAUSS               = (0b00 << 5),  // +/- 2 gauss
+	LSM9DS0_MAGGAIN_4GAUSS               = (0b01 << 5),  // +/- 4 gauss
+	LSM9DS0_MAGGAIN_8GAUSS               = (0b10 << 5),  // +/- 8 gauss
+	LSM9DS0_MAGGAIN_12GAUSS              = (0b11 << 5)   // +/- 12 gauss
+} lsm9ds0MagGain_t;
+
+typedef enum
+{
+	LSM9DS0_GYROSCALE_245DPS             = (0b00 << 4),  // +/- 245 degrees per second rotation
+	LSM9DS0_GYROSCALE_500DPS             = (0b01 << 4),  // +/- 500 degrees per second rotation
+	LSM9DS0_GYROSCALE_2000DPS            = (0b10 << 4)   // +/- 2000 degrees per second rotation
+} lsm9ds0GyroScale_t;
+
+typedef struct vector_s
+{
+	float x;
+	float y;
+	float z;
+} lsm9ds0Vector_t;
+
+void lsm9ds0_init_sensor(uint32_t i2c, uint8_t sensor);
+/* obsolete */
 void lsm9ds0_setup_temp(uint32_t i2c, uint8_t sensor);
+
+lsm9ds0Vector_t lsm9ds0_read_accel(uint32_t i2c, uint8_t sensor);
 uint16_t lsm9ds0_read_temp(uint32_t i2c, uint8_t sensor);
 
 #endif
